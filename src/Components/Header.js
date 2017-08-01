e834dba36fa40a18a8c1dac77f09b82f6ebdc24c
Lawrence Lee <lawrence.lee@foxmail.com>
2017-08-01 10:35:45 +0800

    add link on Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,11 +4,14 @@
 import React, { Component } from 'react';
 import logo from '../Images/logo-large.png';
 import './Header.css';
-function click () {
-    console.log('Clicked!');
-    window.open('https://github.com/theseus-online', 'theseus-online - Github');
-}
 export default class Header extends Component {
+    handleClick (target) {
+        if (target === 'blog') {
+            window.open('http://blog.theseus.online/', 'Theseus Blog')
+        } else if (target === 'github') {
+            window.open('https://github.com/theseus-online', 'theseus-online - Github');
+        }
+    }
     render () {
         return (
             <div className="header">
@@ -16,9 +19,9 @@ export default class Header extends Component {
                     <li className="logo"><img alt="" src={ logo } /></li>
                     <li className="nav-item nav-item-selected"><i className="icon ion-ios-home-outline" /> Home</li>
                     <li className="nav-item"><i className="icon ion-ios-bookmarks-outline" /> Tutorial</li>
-                    <li className="nav-item"><i className="icon ion-social-rss-outline" /> Blog</li>
+                    <li className="nav-item" onClick={ this.handleClick.bind(this, 'blog') }><i className="icon ion-social-rss-outline" /> Blog</li>
                     <li className="nav-item"><i className="icon ion-ios-information-outline" /> About</li>
-                    <li className="nav-item" onClick={ click }><i className="icon ion-social-github" /> Github</li>
+                    <li className="nav-item" onClick={ this.handleClick.bind(this, 'github') }><i className="icon ion-social-github" /> Github</li>
                 </ul>
             </div>
         )
